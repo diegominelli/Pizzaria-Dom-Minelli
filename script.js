79394bed3539037c1b878a93c0be16990f089116
Diego Minelli <dodi@gft.com>
2021-08-19 15:24:51 -0300

    Montando o modal ao clicar na pizza

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ pizzaJson.map((item, index) => {
   ).innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
   pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+  pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    c(".pizzaWindowArea").style.opacity = 0.5;
+    c(".pizzaWindowArea").style.display = "flex";
+    setTimeout(() => {
+      c(".pizzaWindowArea").style.opacity = 1;
+    }, 200);
+  });
 
   c(".pizza-area").append(pizzaItem);
 });
